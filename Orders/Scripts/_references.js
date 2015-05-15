f5afa10fd88c8d88a846d6ce8ee5b0258cd1799c
Ruslan Kutynko <kutynko@hotmail.com>
2015-05-15 16:51:20 +0300

    cleared garbage

--- Orders/Scripts/_references.js
+++ Orders/Scripts/_references.js
@@ -2,6 +2,5 @@
 /// <reference path="angular-mocks.js" />
 /// <reference path="angular.js" />
 /// <reference path="app.js" />
-/// <reference path="ng-grid-1.3.2.js" />
-/// <reference path="select.js" />
 /// <reference path="tree-view.js" />
+/// <reference path="underscore.js" />
